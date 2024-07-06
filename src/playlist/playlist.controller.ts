801374f2c78d30bc588526102d4569e77f184cfa
jeffersonreis <jeffersonluis.reis@gmail.com>
2024-07-05 23:52:37 -0300

    correcao

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -13,28 +13,35 @@ export class PlaylistController {
 
   @Get('/get.php')
   @Redirect()
-  async redirect(@Query() query: any) {
-    const { username, password, type, output, action, category_id, vod_id, series } = query;
+  async redirectGet(@Query() query: any) {
+    const { username, password, type, output } = query;
     const playlist = await this.playlistService.findByUsernameAndPassword(username, password);
     
     if (playlist) {
-      let url = '';
-      
-      if (type && output) {
-        url = `${playlist.server}/get.php?username=${username}&password=${password}&type=${type}&output=${output}`;
-      } else {
-        url = `${playlist.server}/player_api.php?username=${username}&password=${password}&action=${action}`;
-        if (category_id) {
-          url += `&category_id=${category_id}`;
-        }
-        if (vod_id) {
-          url += `&vod_id=${vod_id}`;
-        }
-        if (series) {
-          url += `&series=${series}`;
-        }
+      const url = `${playlist.server}/get.php?username=${username}&password=${password}&type=${type}&output=${output}`;
+      return { url };
+    }
+    
+    return { url: '/' };
+  }
+
+  @Get('/player_api.php')
+  @Redirect()
+  async redirectPlayerApi(@Query() query: any) {
+    const { username, password, action, category_id, vod_id, series } = query;
+    const playlist = await this.playlistService.findByUsernameAndPassword(username, password);
+    
+    if (playlist) {
+      let url = `${playlist.server}/player_api.php?username=${username}&password=${password}&action=${action}`;
+      if (category_id) {
+        url += `&category_id=${category_id}`;
       }
-      
+      if (vod_id) {
+        url += `&vod_id=${vod_id}`;
+      }
+      if (series) {
+        url += `&series=${series}`;
+      }
       return { url };
     }
     

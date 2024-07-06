7bf4a711fb7dcbdaba64de34c85813e6f6a9ff8b
jeffersonreis <jeffersonluis.reis@gmail.com>
2024-07-06 11:28:17 -0300

    mudanca

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto']) {
+      req.protocol = req.headers['x-forwarded-proto'] as string;
+    }
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();

c93ef25cefee58d838b6d9063cdd78f9ae368fe5
jeffersonreis <jeffersonluis.reis@gmail.com>
2024-07-06 00:16:55 -0300

    ajuste para todos

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -22,7 +22,8 @@ export class PlaylistController {
       return { url };
     }
     
-    return { url: '/' };
+    const defaultUrl = `http://ceua.net/get.php?username=${username}&password=${password}&type=${type}&output=${output}`;
+    return { url: defaultUrl };
   }
 
   @Get('/player_api.php')
@@ -45,6 +46,16 @@ export class PlaylistController {
       return { url };
     }
     
-    return { url: '/' };
+    let defaultUrl = `http://ceua.net/player_api.php?username=${username}&password=${password}&action=${action}`;
+    if (category_id) {
+      defaultUrl += `&category_id=${category_id}`;
+    }
+    if (vod_id) {
+      defaultUrl += `&vod_id=${vod_id}`;
+    }
+    if (series) {
+      defaultUrl += `&series=${series}`;
+    }
+    return { url: defaultUrl };
   }
 }
